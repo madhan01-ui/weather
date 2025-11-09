4a76d12278a2763c0219f48da5bea1ef4a709dcc
madhan01-ui <maddymadhan1207@gmail.com>
2025-11-09 10:59:23 +0530

    Update rain.js

--- rain.js
+++ rain.js
@@ -8,16 +8,16 @@ function resizeCanvas() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 }
-
 window.addEventListener("resize", resizeCanvas);
 resizeCanvas();
 
 function createRainDrop() {
   return {
     x: Math.random() * canvas.width,
-    y: 0,
-    length: Math.random() * 15 + 10,
-    speed: Math.random() * 5 + 2,
+    y: Math.random() * -canvas.height,
+    length: Math.random() * 20 + 10,
+    speed: Math.random() * 6 + 3,
+    opacity: Math.random() * 0.4 + 0.2,
   };
 }
 
@@ -31,22 +31,21 @@ function drawRain() {
     ctx.moveTo(drop.x, drop.y);
     ctx.lineTo(drop.x, drop.y + drop.length);
     ctx.stroke();
+    drop.y += drop.speed;
+    if (drop.y > canvas.height) {
+      drop.y = -20;
+      drop.x = Math.random() * canvas.width;
+    }
   });
-
-  rainDrops = rainDrops.map((drop) => ({
-    ...drop,
-    y: drop.y + drop.speed,
-  })).filter((drop) => drop.y < canvas.height);
-
-  while (rainDrops.length < 100) {
-    rainDrops.push(createRainDrop());
-  }
 
   animation = requestAnimationFrame(drawRain);
 }
 
 function startRain() {
-  if (!animation) drawRain();
+  if (!animation) {
+    rainDrops = Array(120).fill().map(createRainDrop);
+    drawRain();
+  }
 }
 
 function stopRain() {
